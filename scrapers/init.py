abe565546c40c9cb7ab9a8ddb7e93a66d56b20d3
paprikacc <133728579+paprikacc@users.noreply.github.com>
2026-04-16 01:30:05 -0300

    Create init.py

---
+++ scrapers/init.py
@@ -0,0 +1,15 @@
+from .carrefour_scraper import CarrefourScraper
+from .jumbo_scraper import JumboScraper
+from .dia_scraper import DiaScraper
+from .coto_scraper import CotoScraper
+from .makro_scraper import MakroScraper
+from .modo_scraper import ModoScraper
+
+__all__ = [
+    'CarrefourScraper',
+    'JumboScraper',
+    'DiaScraper',
+    'CotoScraper',
+    'MakroScraper',
+    'ModoScraper'
+]
